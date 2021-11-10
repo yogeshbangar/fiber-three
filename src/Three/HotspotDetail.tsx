713cb7361621af2729a91e7581bbc76fee624180
yogesh2503 <yogesh@inspify.com>
2021-11-10 16:31:00 +0800

    add touch ssp pro file

---
+++ src/Three/HotspotDetail.tsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from 'react';
+import { useFrame } from 'react-three-fiber';
+import * as THREE from 'three';
+import { Mesh } from 'three';
+
+const TIME_TRAVEL = 1000;
+
+export default function HotspotDetail({ TWEEN }: { TWEEN }) {
+  const ref = useRef();
+  const points = [];
+  points.push(new THREE.Vector3(-10.5, 0, -1.5));
+  points.push(new THREE.Vector3(-0.5, -1, -1.5));
+  points.push(new THREE.Vector3(1.5, 0, -15));
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+  const textureLoader = new THREE.TextureLoader();
+  useEffect(() => {
+    const detailMesh:Mesh = ref?.current;
+    const textureDetail = textureLoader.load(
+      'https://hututusoftwares.com/images/team_02.jpg'
+    );
+    const showdetail = detailMesh.children.find(
+      ({ name }) => name === 'showdetail'
+    )?.['material'];
+  }, []);
+
+  return (
+    <group ref={ref}>
+      <mesh position={[2.3, 1.3, -1]} scale={[2, 2.5, 2]} name={'showdetail'}>
+        <planeGeometry attach="geometry" args={[1.0, 1.0]} />
+        <meshBasicMaterial attach="material" />
+      </mesh>
+      <line name={'line'}>
+        <bufferGeometry attach="geometry" {...lineGeometry} />
+        <lineBasicMaterial attach="material" color="green" linewidth={30} />
+      </line>
+    </group>
+  );
+}
--- src/Three/Line.js
+++ src/Three/Line.js
@@ -1,73 +1,175 @@
-import {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-  useLayoutEffect,
-} from "react";
-
-import { MeshLine, MeshLineMaterial, MeshLineRaycast } from "three.meshline";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
 import * as THREE from "three";
-import LineEffect from "./LineEffect";
 
+const TIME_TRAVEL = 1000;
+extend({ OrbitControls });
 function Box(props) {
   const ref = useRef();
-  const threeRef = useThree();
+  const points = [];
+  points.push(new THREE.Vector3(0.5, 0, 1.5));
+  points.push(new THREE.Vector3(0.5, 0, 1.5));
+  points.push(new THREE.Vector3(0.5, 0, 1.5));
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
   const textureLoader = new THREE.TextureLoader();
   const textureRing = textureLoader.load(
+    "https://hututusoftwares.com/images/team_02.jpg"
+  );
+  const textureDetail = textureLoader.load(
     "https://hututusoftwares.com/images/team_02.jpg"
   );
   textureRing.wrapS = textureRing.wrapT = THREE.RepeatWrapping;
   textureRing.repeat.x = 0.1;
   textureRing.offset.x = 100;
   useFrame((state, delta) => {
-    if (textureRing&& ref?.current?.scale) {
-        // 
+    if (textureRing && ref?.current?.scale) {
       if (textureRing.repeat.x < 1) {
-        textureRing.repeat.x += .01;
-        ref.current.scale.x=textureRing.repeat.x;
-        if(textureRing.repeat.x>1){
-            ref.current.scale.x = 1;
-            textureRing.repeat.x = 1;
+        const result = ref?.current.children.find(
+          ({ name }) => name === "detail"
+        );
+        textureRing.repeat.x += 0.01;
+        if (result) result.scale.x = textureRing.repeat.x;
+        if (textureRing.repeat.x > 1) {
+          ref.current.scale.x = 1;
+          textureRing.repeat.x = 1;
         }
       }
+      const line = ref?.current.children.find(({ name }) => name === "line")
+        ?.geometry?.attributes?.position;
+      if (line) {
+        line.array[0] = points[0].x;
+        line.array[1] = points[0].y;
+        line.array[2] = points[0].z;
+        line.array[3] = points[1].x;
+        line.array[4] = points[1].y;
+        line.array[5] = points[1].z;
+        line.array[6] = points[2].x;
+        line.array[7] = points[2].y;
+        line.array[8] = points[2].z;
+        line.needsUpdate = true;
+      }
     }
+    TWEEN?.update();
   });
+
+  const setLast = (lastPos) => {
+    new TWEEN.Tween(points[2])
+      .to(
+        {
+          x: lastPos.x,
+          y: lastPos.y,
+          z: lastPos.z,
+        },
+        TIME_TRAVEL
+      )
+      .onComplete(() => {
+        console.log("onComplete");
+      })
+      .start();
+
+    const showdetail = ref?.current.children.find(
+      ({ name }) => name === "showdetail"
+    );
+    console.log("showdetail-> ", ref?.current);
+    new TWEEN.Tween(showdetail.material)
+      .to(
+        {
+          opacity: 1,
+        },
+        TIME_TRAVEL
+      )
+      .onComplete(() => {
+        console.log("onComplete");
+      })
+      .start();
+  };
+
+  const setFocus = (centerPos, lastPos) => {
+    console.log("setFocus", TWEEN);
+    if (!TWEEN) return;
+    new TWEEN.Tween(points[1])
+      .to(
+        {
+          x: centerPos.x,
+          y: centerPos.y,
+          z: centerPos.z,
+        },
+        TIME_TRAVEL
+      )
+      .onComplete(() => {
+        console.log("onComplete");
+      })
+      .start();
+    new TWEEN.Tween(points[2])
+      .to(
+        {
+          x: centerPos.x,
+          y: centerPos.y,
+          z: centerPos.z,
+        },
+        TIME_TRAVEL
+      )
+      .onComplete(() => {
+        setLast(lastPos);
+      })
+      .start();
+  };
+  useEffect(() => {
+    setFocus({ x: 1, y: 1, z: 1 }, { x: 3.6, y: 1, z: 1 });
+  }, []);
+
   return (
-    <mesh ref={ref} scale={[0.1,1,1]}>
-      <boxGeometry args={[1.0, 1.0, 1.0]} />
-      <meshStandardMaterial map={textureRing} attach="material" />
-    </mesh>
+    <group ref={ref}>
+      <mesh scale={[0.1, 1, 1]} name={"detail"}>
+        <planeGeometry args={[1.0, 1.0]} />
+        <meshStandardMaterial map={textureRing} attach="material" />
+      </mesh>
+      <mesh position={[2.3, 1.3, 1]} scale={[2, 0.5, 2]} name={"showdetail"}>
+        <planeGeometry args={[1.0, 1.0]} />
+        <meshBasicMaterial
+          map={textureDetail}
+          attach="material"
+          transparent={true}
+          opacity={0}
+        />
+      </mesh>
+      <line name={"line"}>
+        <bufferGeometry attach="geometry" {...lineGeometry} />
+        <lineBasicMaterial color="red" linewidth={3} />
+      </line>
+    </group>
   );
 }
 function DrawLine({ start, end }) {
   const ref = useRef();
-
   const points = [];
   points.push(new THREE.Vector3(-0.5, 0, 1.5));
-  points.push(new THREE.Vector3(0.5, 0.5, 1.5));
-  points.push(new THREE.Vector3(0.5, 0, -105));
-  useLayoutEffect(() => {
-    ref.current.geometry.setFromPoints(points);
-  }, []);
+  points.push(new THREE.Vector3(1.5, 0.5, 1.5));
+  points.push(new THREE.Vector3(-2.5, 0, -5));
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
 
   useFrame((state, delta) => {
-    if (ref?.current?.rotation) {
-    //   ref.current.geometry.attributes.position.array[0] += 0.01;
-    //   ref.current.geometry.attributes.position.needsUpdate = true;
-    }
+    points[0].x++;
+    ref.current.geometry.attributes.position.array[0] = points[0].x;
+    // ref.current.geometry.attributes.position.needsUpdate = true;
   });
 
   return (
     <line ref={ref}>
-      <bufferGeometry />
+      <bufferGeometry attach="geometry" {...lineGeometry} />
       <lineBasicMaterial color="hotpink" linewidth={1} />
     </line>
   );
 }
-
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  return <orbitControls args={[camera, domElement]} />;
+};
 export default function Line() {
   return (
     <Canvas
@@ -76,10 +178,10 @@ export default function Line() {
       gl={{ antialias: true }}
     >
       <color attach="background" args={[0, 0, 0]} />
+      <CameraControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box />
-      <DrawLine start={[0, 0, 0]} end={[1, 0, 0]} />
     </Canvas>
   );
 }
